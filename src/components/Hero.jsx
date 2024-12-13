07910b28bbb5d640010256db0f32d38218739791
lukesolgg <lukesolgg@gmail.com>
2024-12-13 19:49:19 +0000

    edited images to match style better

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import { HERO_CONTENT } from "../constants";
-import profilePic from "../assets/kevinRushProfile.png";
+import profilePic from "../assets/aigeneratedimagelol1.jpg";
 import { motion } from "motion/react"
 
 const container = (delay) => ({
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ import { FaInstagram } from "react-icons/fa";
 const Navbar = () => {
   return <nav className="mb-20 flex items-center justify-between py-6">
     <div className="flex flex-srhink-0 items-center">
-        <img className="mx-2 w-10" src={logo} alt="logo" />
+        <h1 className="mx-2 w-10 text-5xl">LK</h1>
     </div>
     <div className="m-8 flex items-center justify-center gap-4 text-2xl">
         <FaLinkedin />
